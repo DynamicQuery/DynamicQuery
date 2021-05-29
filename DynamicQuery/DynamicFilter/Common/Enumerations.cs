d82c44146e963ebb89c68edf90f4f33d59660819
Muhammad Razali <wireless-@hotmail.com>
2021-05-29 08:10:00 +0800

    Fixed comments - typo

--- DynamicQuery/DynamicFilter/Common/Enumerations.cs
+++ DynamicQuery/DynamicFilter/Common/Enumerations.cs
@@ -5,7 +5,7 @@ namespace DynamicFilter.Common
     /// <summary>
     /// Defines how filter statements will be connected to each other.
     /// 
-    /// If you have 2 filter statements {F1, F2}, we need to attache a Connector, eg 'AND',
+    /// If you have 2 filter statements {F1, F2}, we need to attach a Connector, eg 'AND',
     /// to F2. F2 with its connector 'AND' will now be able to attach to F1, just like a leggo,
     /// to produce F1 'AND' F2
     /// </summary>
