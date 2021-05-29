ff94cad0c3439d8b649c4f176fd6de2e4609e187
Muhammad Razali <wireless-@hotmail.com>
2021-05-29 20:16:05 +0800

    Added basic test file

---
+++ DynamicQuery/Tests/DynamicFilter/Helpers/NestedListsHelperTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class NestedListsHelperTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
