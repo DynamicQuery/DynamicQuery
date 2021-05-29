e73cc486f22d8d0907f3b1bf5b9d1ddad818c731
Muhammad Razali <wireless-@hotmail.com>
2021-05-29 08:08:53 +0800

    Added TypeGroup enum

--- DynamicQuery/DynamicFilter/Common/Enumerations.cs
+++ DynamicQuery/DynamicFilter/Common/Enumerations.cs
@@ -1,4 +1,6 @@
-﻿namespace DynamicFilter.Common
+﻿using System;
+
+namespace DynamicFilter.Common
 {
     /// <summary>
     /// Defines how filter statements will be connected to each other.
@@ -23,5 +25,42 @@
         /// Only applied to the first filter statement, as it has no connectors
         /// </summary>
         None
+    }
+
+    /// <summary>
+    /// Groups types into simple groups and map the supported operations to each group.
+    /// </summary>
+    [Flags]
+    public enum TypeGroup
+    {
+        /// <summary>
+        /// Default type group, only supports EqualTo and NotEqualTo.
+        /// </summary>
+        Default = -1,
+
+        /// <summary>
+        /// Supports all text related operations.
+        /// </summary>
+        Text = 1,
+
+        /// <summary>
+        /// Supports all numeric related operations.
+        /// </summary>
+        Number = 2,
+
+        /// <summary>
+        /// Supports boolean related operations.
+        /// </summary>
+        Boolean = 4,
+
+        /// <summary>
+        /// Supports all date related operations.
+        /// </summary>
+        Date = 8,
+
+        /// <summary>
+        /// Supports nullable related operations.
+        /// </summary>
+        Nullable = 16
     }
 }
