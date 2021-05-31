9a5446a3dc5be6e4915b4358b6b57787c68e113a
Muhammad Razali <wireless-@hotmail.com>
2021-05-31 20:47:04 +0800

    Added extensions and selectquerybuilder

---
+++ DynamicQuery/DynamicSelect/DynamicSelectExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace DynamicSelect
+{
+    public static class DynamicSelectExtensions
+    {
+        public static IQueryable ProjectToDynamic<T>(this IQueryable<T> queryable, IEnumerable<string> propertyIds)
+            where T : class
+        {
+            Node root = new Node();
+
+            foreach (string propertyId in propertyIds)
+            {
+                root.Add(propertyId);
+            }
+
+            IncludeQueryBuilder includeQueryBuilder = new IncludeQueryBuilder();
+            List<string> includes = includeQueryBuilder.Build(root).ToList();
+            includes.ForEach(include =>
+            {
+                queryable = queryable.Include(include);
+            });
+
+            DynamicSelectQueryBuilder selectQueryBuilder = new DynamicSelectQueryBuilder();
+
+            string selectQuery = selectQueryBuilder.Build(root);
+
+            return queryable.Select(selectQuery);
+        }
+    }
+}
---
+++ DynamicQuery/DynamicSelect/DynamicSelectQueryBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace DynamicSelect
+{
+    public class DynamicSelectQueryBuilder
+    {
+        public string Build(Node node)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (node.Type == Node.NodeType.ROOT) //root node
+            {
+                foreach (var childNode in node.ChildNodes)
+                {
+                    string build = Build(childNode);
+                    sb.Append(build);
+                }
+
+                string build2 = sb.ToString();
+                build2 = build2.Substring(0, build2.Length - 1);
+                string ret = $"new {{ {build2 } }}";
+                return ret;
+            }
+            else if (node.Type == Node.NodeType.PROPERTY) // leaf node
+            {
+                string ret = $"{node.Projection.PropertyId} as {node.Projection.DisplayName},";
+                return ret;
+            }
+            else if (node.Type == Node.NodeType.OBJECT)
+            {
+                foreach (var childNode in node.ChildNodes)
+                {
+                    string build = Build(childNode);
+                    build = $"{node.Projection.DisplayName}.{build}";
+                    sb.Append(build);
+                }
+
+                string build2 = sb.ToString();
+                build2 = build2.Substring(0, build2.Length - 1);
+                string ret = $"new {{ { build2} }} as {node.Projection.DisplayName},";
+                return ret;
+            }
+            else
+            {
+                foreach (var childNode in node.ChildNodes)
+                {
+                    string build = Build(childNode);
+                    build = $"{build}";
+                    sb.Append(build);
+                }
+
+                string build2 = sb.ToString();
+                build2 = build2.Substring(0, build2.Length - 1);
+                string ret = $"{node.Projection.DisplayName}.Select(new {{ {build2} }}) as {node.Projection.DisplayName},";
+                return ret;
+            }
+        }
+    }
+}
