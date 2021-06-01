70e68f3267ec39cfe477edbfa988b35dc3b44793
Muhammad Razali <wireless-@hotmail.com>
2021-06-01 14:11:10 +0800

    Created Projection

---
+++ DynamicQuery/DynamicQuery/QueryBuilder/Projection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicQuery.QueryBuilder
+{
+    public class Projection
+    {
+        public List<string> Selections { get; set; } = new List<string>();
+        public int PageSize { get; set; } = 0;
+        public int Page { get; set; } = 0;
+
+    }
+}
