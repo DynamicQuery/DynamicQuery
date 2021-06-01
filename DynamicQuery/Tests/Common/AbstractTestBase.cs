906bcca5b90a8afe5d2f6528a7b24d6f1e83ef14
Muhammad Razali <wireless-@hotmail.com>
2021-06-01 22:20:14 +0800

    Added abstractTestBase

---
+++ DynamicQuery/Tests/Common/AbstractTestBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Tests.Common.Persistence;
+
+namespace Tests.Common
+{
+    public abstract class AbstractTestBase
+    {
+        protected SeedDbContext _seedDbContext;
+
+        public AbstractTestBase()
+        {
+            DbContextOptionsBuilder<SeedDbContext> dbContextOptionsBuilder =
+                new DbContextOptionsBuilder<SeedDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            _seedDbContext = new SeedDbContext(dbContextOptionsBuilder.Options);
+
+            _seedDbContext.Persons.AddRange(SeedDbContext.Seed());
+            _seedDbContext.SaveChanges();
+
+        }
+    }
+}
