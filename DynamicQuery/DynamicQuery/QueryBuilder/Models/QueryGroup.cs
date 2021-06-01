3ae8de521dbb719f57eac22c9bc1f824340c8424
Muhammad Razali <wireless-@hotmail.com>
2021-06-01 14:26:42 +0800

    Added QueryGroup and Query Logic

---
+++ DynamicQuery/DynamicQuery/QueryBuilder/Models/QueryGroup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicQuery.QueryBuilder.Models
+{
+    public class QueryGroup
+    {
+        public List<Query> Queries { get; set; } = new List<Query>();
+    }
+}
---
+++ DynamicQuery/DynamicQuery/QueryBuilder/Models/QueryLogic.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DynamicQuery.QueryBuilder.Models
+{
+    public class QueryLogic
+    {
+        public List<QueryGroup> QueryGroups { get; set; } = new List<QueryGroup>();
+
+        public Projection Projection { get; set; } = new Projection();
+
+        public string AsJson(bool indented = true)
+            => JsonConvert.SerializeObject(this, indented ? Formatting.Indented : Formatting.None);
+    }
+}
